5a600d27c007ea5a60140a2067fd9f1b0f82bde0
David Agbaniyaka <fiyin.agba@gmail.com>
2024-01-27 07:49:52 -0500

    Changes

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header"
+import MainComponent from "./components/MainComponent"
+
+const App = () => {
+
+  return (
+    <>
+       <Header />
+       <MainComponent /> 
+    </>
+  )
+}
+
+export default App
